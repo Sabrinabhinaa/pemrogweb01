e324ae0e04ae6b87050fa87a0bce71532882668b
Sabrina Ramadhani <115170311+Sabrinabhinaa@users.noreply.github.com>
2022-12-20 15:02:14 +0700

    Add files via upload

---
+++ Sabrina-TI9/praktikum-11/assets/js/script.js
@@ -0,0 +1,53 @@
+// menangkap elemen dengan menggunakan looping
+document.querySelectorAll('#option a').forEach((anchor) => {
+    anchor.addEventListener('click', (element) => {
+        computerChoice (element);
+    })
+})
+
+function computerChoice(element){
+    //menangkap pilihan user
+    let pilihanUser = element.target.innerText;
+
+    //menangkap pilihan komputer pada result
+    let pilihanKomputer = document.querySelector('#result2');
+
+    //membuat pilihan untuk komputer
+    let choices = ['Rock', 'Paper', 'Scissors']
+
+    //membuat pilihan random untuk komputer
+    pilihanKomputer.innerHTML = choices[Math.round(Math.random() * choices.length)];
+    pilihanKomputer = pilihanKomputer.innerHTML;
+
+
+    // jika user yg menang 
+    if ( pilihanUser == 'Rock' && pilihanKomputer == 'Scissors'){
+        setTimeout(alert('You Win'), 100);
+    }
+
+    if ( pilihanUser == 'Paper' && pilihanKomputer == 'Rock'){
+        setTimeout(alert('You Win'), 100);
+    }
+
+    if ( pilihanUser == 'Scissors' && pilihanKomputer == 'Paper'){
+        setTimeout(alert('You Win'), 100);
+    }
+
+    //jika komputer yang menang
+    if ( pilihanUser == 'Scissors' && pilihanKomputer == 'Rock'){
+        setTimeout(alert('You Win'), 100);
+    }
+
+    if ( pilihanUser == 'Rock' && pilihanKomputer == 'Paper'){
+        setTimeout(alert('You Win'), 100);
+    }
+
+    if ( pilihanUser == 'Paper' && pilihanKomputer == 'Scissors'){
+        setTimeout(alert('You Win'), 100);
+    }
+
+    //jika draw atau seri
+    if(pilihanUser == pilihanKomputer){
+        setTimeout(alert('Draw'), 100);
+    }
+}
